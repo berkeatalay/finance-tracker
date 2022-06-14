b6518f5c8cdb659fd24624e91e17b7b5466aba79
Berke <tormorkor@outlook.com>
2022-06-14 22:41:27 +0300

    feat: working navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Switch,Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
         <BrowserRouter>
+        <Navbar />
           <Switch>
             <Route exact path={"/"}>
               <Home />
